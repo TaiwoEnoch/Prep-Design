e8fcea59fb920321800c4e52b9585cd9dc29dfd3
TaiwoEnoch <taiwoenoch0@gmail.com>
2024-08-26 08:34:52 +0100

    Make use of useNavigation hook in react for better user experience

--- src/components/Navigations/Nav.jsx
+++ src/components/Navigations/Nav.jsx
@@ -1,12 +1,12 @@
-import { Link, useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import HomeIcon from '@mui/icons-material/Home';
 import MessageIcon from '@mui/icons-material/Message';
 import CreateNewFolderIcon from '@mui/icons-material/CreateNewFolder';
 import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
 
-
 const Nav = () => {
   const location = useLocation();
+  const navigate = useNavigate();
 
   // Determine if the current path matches the given route
   const isActive = (path) => location.pathname === path;
@@ -17,52 +17,52 @@ const Nav = () => {
         <section className='w-full md:w-[100%] max-w-full bg-[#fbfbfb] border-t-2 border-[#e5e5e5] py-4 mx-auto flex justify-center'>
           <ul className='flex gap-10 items-center'>
             <li>
-              <Link 
-                to="/" 
-                className='flex flex-col font-extralight text-[10px] items-center '
+              <button 
+                onClick={() => navigate("/", { replace: true })}
+                className='flex flex-col font-extralight text-[10px] items-center'
               >
-              <HomeIcon 
-                alt="home" 
-                className={`w-[25px] ${isActive('/') ? 'text-[#fcdd00]' : 'text-gray-400'}`} 
-              />
+                <HomeIcon 
+                  alt="home" 
+                  className={`w-[25px] ${isActive('/') ? 'text-[#fcdd00]' : 'text-gray-400'}`} 
+                />
                 <span>Home</span>
-              </Link>
+              </button>
             </li>
             <li>
-              <Link 
-                to="/messages" 
+              <button 
+                onClick={() => navigate("/messages", { replace: true })}
                 className='flex font-extralight text-[10px] flex-col items-center'
               >
-              <MessageIcon 
-                alt="messages" 
-                className={`w-[20px] ${isActive('/messages') ? 'text-[#fcdd00]' : 'text-gray-400'}`} 
-              />
+                <MessageIcon 
+                  alt="messages" 
+                  className={`w-[20px] ${isActive('/messages') ? 'text-[#fcdd00]' : 'text-gray-400'}`} 
+                />
                 <span>Messages</span>
-              </Link>
+              </button>
             </li>
             <li>
-              <Link 
-                to="/create" 
+              <button 
+                onClick={() => navigate("/create", { replace: true })}
                 className='flex font-extralight text-[10px] flex-col items-center'
               >
-              <CreateNewFolderIcon 
-                alt="create" 
-                className={`w-[20px] ${isActive('/create') ? 'text-[#fcdd00]' : 'text-gray-400'}`} 
-              />
+                <CreateNewFolderIcon 
+                  alt="create" 
+                  className={`w-[20px] ${isActive('/create') ? 'text-[#fcdd00]' : 'text-gray-400'}`} 
+                />
                 <span>Create</span>
-              </Link>
+              </button>
             </li>
             <li>
-              <Link 
-                to="/profiles" 
+              <button 
+                onClick={() => navigate("/profiles", { replace: true })}
                 className='flex font-extralight text-[10px] flex-col items-center'
               >
-              <PersonOutlineIcon 
-                alt="profile" 
-                className={`w-[20px] ${isActive('/profiles') ? 'text-[#fcdd00]' : 'text-gray-400'}`} 
-              />
+                <PersonOutlineIcon 
+                  alt="profile" 
+                  className={`w-[20px] ${isActive('/profiles') ? 'text-[#fcdd00]' : 'text-gray-400'}`} 
+                />
                 <span>Profile</span>
-              </Link>
+              </button>
             </li>
           </ul>
         </section>
